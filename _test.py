96e516c04719eb5facf0fee45f5de362ac382917
Conner <conner.hunihan@gmail.com>
2018-03-04 11:35:09 -0800

    clean up _test.py

--- _test.py
+++ _test.py
@@ -1,26 +1,5 @@
 from NextBus import NextBusAPI
 
-# class BusStatus(object):
-	
-# 	def __init__(self):
-# 		''' 
-# 		initiate with values for the current stop 
-# 		'''
-# 		self.stopInput = "E 18th St & 12th Av"
-# 		self.routeInput = "14"
-# 		self.directionInput = "West Oakland BART"
-
-# 	def getNextArrival(self, bus):
-# 		''' 
-# 		generate and return minutes until next arrival
-# 		'''
-# 		results = bus.BartRoutesResponse(stopInput = stopInput, routeInput = routeInput, directionInput = directionInput)[0]
-# 		minutesUntilArrival = results[directionInput][0]
-# 		return minutesUntilArrival
-
-# NextBus = NextBusAPI()
-# print(BusStatus.getNextArrival(NextBus))
-
 # ORIGINAL PYTHON CODE
 NextBus = NextBusAPI()
 route = '14'
